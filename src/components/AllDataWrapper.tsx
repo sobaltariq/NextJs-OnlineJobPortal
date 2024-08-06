9f954826ec15455d657a2302079c05ab0fd943f9
SobalWork <sobalwork@gmail.com>
2024-08-06 14:42:43 -0700

    created main loader

--- src/components/AllDataWrapper.tsx
+++ src/components/AllDataWrapper.tsx
@@ -5,16 +5,50 @@ import Footer from "./footer/Footer";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import { initializeAuthState } from "@/redux/features/auth/authSlice";
+import Image from "next/image";
+
+import LoadingImg from "../assets/Loader.svg";
+import { setAppMainLoader } from "@/redux/features/gobalSlicer";
+import { waitSec } from "@/utils/CommonWait";
 
 const AllDataWrapper: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const isChat = useSelector((state: RootState) => state.chat.isChat);
+  const { isLoggedIn, userRole, isSearch } = useSelector(
+    (state: RootState) => state.auth
+  );
+  const { appMainLoader } = useSelector((state: RootState) => state.global);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(initializeAuthState());
   }, [dispatch]);
+
+  useEffect(() => {
+    const setLoader = async () => {
+      if (isLoggedIn) {
+        dispatch(setAppMainLoader(false));
+      }
+      await waitSec(200);
+      dispatch(setAppMainLoader(false));
+    };
+    setLoader();
+  }, []);
+
+  if (appMainLoader) {
+    return (
+      <div className="h-dvh flex justify-center items-center">
+        <Image
+          src={LoadingImg}
+          alt="Loading"
+          height={100}
+          width={100}
+          priority
+        />
+      </div>
+    );
+  }
 
   return (
     <>
--- src/redux/features/gobalSlicer.tsx
+++ src/redux/features/gobalSlicer.tsx
@@ -3,9 +3,11 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface GlobalInterface {
   appStatus: number;
+  appMainLoader: boolean;
 }
 const initialState: GlobalInterface = {
   appStatus: 200,
+  appMainLoader: true,
 };
 
 export const chatSlice = createSlice({
@@ -15,10 +17,13 @@ export const chatSlice = createSlice({
     setAppStatus: (state, action: PayloadAction<number>) => {
       state.appStatus = action.payload;
     },
+    setAppMainLoader: (state, action: PayloadAction<boolean>) => {
+      state.appMainLoader = action.payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setAppStatus } = chatSlice.actions;
+export const { setAppStatus, setAppMainLoader } = chatSlice.actions;
 
 export default chatSlice.reducer;
